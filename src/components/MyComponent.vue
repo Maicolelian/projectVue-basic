6abc0c55539617a0266f9e2fa00eea8f04743c66
MaiElian <elianmuepaz29@gmail.com>
2025-04-24 15:57:02 -0500

    commit inicial

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="my-component">
+        <!-- <h1 v-if="isVisible"> {{ message }}</h1> -->
+        <h1 v-bind:class="{ highlighted: ishighlighted }"> {{ title }}</h1>
+        <button v-on:click="toggleHighlight">Visible</button>
+        <input v-model="message">
+
+        <ul>
+            <li v-for="item in list" v-bind:key="item.id">{{ item.text }}</li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+.highlighted {
+    color: goldenrod;
+}
+</style>
+
+<script>
+export default {
+    name: 'MyComponent',
+    data() {
+        return {
+            message: 'Hola mundo!  Esta es la pagina de inicio!',
+            title: 'Hola!  Esta es el titulo!',
+            ishighlighted: false,
+            isVisible: true,
+            list: [
+                { id: 1, text: 'Item 1' },
+                { id: 2, text: 'Item 2' },
+                { id: 3, text: 'Item 3' }
+            ]
+        };
+    },
+    methods: {
+        toggleVisibility() {
+            this.isVisible = !this.isVisible;
+        },
+        sayHello() {
+            alert(this.message);
+        },toggleHighlight() {
+            this.ishighlighted = !this.ishighlighted;
+        }
+    },
+}
+</script>
---
+++ src/components/SecondComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="app">
+        <h1>{{ message }}</h1>
+        <button @click="changeMessage">Change Message</button>
+    </div>
+</template>
+
+<script>
+import { mapState, mapMutations } from 'vuex';
+export default {
+    computed: {
+        ...mapState(['message'])
+    },
+    methods: {
+        ...mapMutations(['setMessage']),
+        changeMessage() {
+            this.setMessage = 'Helooo eveyone!';
+        }
+    }
+}
+</script>
